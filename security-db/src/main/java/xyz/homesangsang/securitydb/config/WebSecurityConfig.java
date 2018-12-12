1eda35f6b2a59e66470a0a630d4bdbc534435fed
homesangsang <homesangsang@qq.com>
2018-12-12 11:17:33 +0800

    spring security 5.*版本，对数据库密码进行加密，默认密码需要加密

--- security-db/src/main/java/xyz/homesangsang/securitydb/config/WebSecurityConfig.java
+++ security-db/src/main/java/xyz/homesangsang/securitydb/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -15,11 +16,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
-                .antMatchers("/", "/static/**").permitAll()
-                .antMatchers("/users/**").hasRole("ROLE_USER")
+                .antMatchers("/", "/static/**", "/login").permitAll()
+                .antMatchers("/users/**").hasRole("USER")
+                .anyRequest().authenticated()
                 .and()
-        .formLogin().loginPage("/login").defaultSuccessUrl("/index")
-        .and().logout().logoutSuccessUrl("/logout").logoutSuccessUrl("/login");
+                .formLogin().loginPage("/login").defaultSuccessUrl("/index")
+                .and().logout().logoutSuccessUrl("/logout").logoutSuccessUrl("/login");
     }
 
     @Override
--- security-db/src/main/java/xyz/homesangsang/securitydb/controller/HomeController.java
+++ security-db/src/main/java/xyz/homesangsang/securitydb/controller/HomeController.java
@@ -1,7 +1,28 @@
 package xyz.homesangsang.securitydb.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping({"/", "/index"})
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/user")
+    public String uesr() {
+        return "user";
+    }
+
+    @RequestMapping("/header")
+    public String header() {
+        return "header";
+    }
 }

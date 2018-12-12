3ec3037dba9a2b3a405f6fea1e21ba424f4dc1d8
homesangsang <homesangsang@qq.com>
2018-12-12 11:14:56 +0800

    spring security 5.*版本，默认密码需要加密

--- security-no-db/src/main/java/xyz/homesangsang/securitynodb/config/WebSecurityConfig.java
+++ security-no-db/src/main/java/xyz/homesangsang/securitynodb/config/WebSecurityConfig.java
@@ -1,18 +1,21 @@
 package xyz.homesangsang.securitynodb.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
+    /*@Bean
     @Override
     protected UserDetailsService userDetailsService() {
         UserDetails user = User.withDefaultPasswordEncoder()
@@ -21,6 +24,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .roles("USER")
                 .build();
         return new InMemoryUserDetailsManager(user);
+    }*/
+
+    /**
+     * 升级到springsecurity5以后，需要加PasswordEncoder来加密密码
+     * @param auth
+     * @throws Exception
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        System.out.println(passwordEncoder.encode("pwd"));
+        auth.inMemoryAuthentication().withUser("user").password(passwordEncoder.encode("pwd")).roles("USER");
     }
 
     @Override
